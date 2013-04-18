a5f8e96ed0cb77e210cbb02e5976f5ac79d52458
epochus <jacksonwu.zg@gmail.com>
2013-04-18 15:22:10 -0500

    2nd commit

---
+++ src/body_scanning.cpp
@@ -0,0 +1,8 @@
+/* Retrieves transform data from openni_tracker and puts it into a text file.
+ *
+ * This was created for the BWI Body Scanning Project for CS378 @ The University of Texas at Austin
+ * Author: Zhen Guang Wu
+ *
+ * */
+
+#include "ros/ros.h"
